c34e091f6690ce77178e10fe50a43ac23bbe08dd
GanesanNS <ganesan@arivuventures.com>
2022-03-09 11:20:59 +0530

    new branch change

---
+++ Poly.java
@@ -0,0 +1,8 @@
+public class Poly{
+    public static void main(String[] args) {
+        System.out.println("welcome");
+        System.out.println("now i am pulling to local repo");
+        System.out.println("now i am in new branch :");
+        System.out.println("new branch");
+    } 
+}
